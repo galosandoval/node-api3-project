af3a8280ff11f14c62629b2ee886e60f71ec8ccd
Galo Sandoval <galosan92@gmail.com>
2020-09-02 16:03:48 -0700

    added the first middleware lol

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 // code away!
+const server = require('./server')
+server.listen(3000, () => console.log("Listening on port 3000"))
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+const helmet = require("helmet");
+const userRouter = require("./users/userRouter");
+const postRouter = require('./posts/postRouter')
 
-server.get('/', (req, res) => {
+server.use(helmet());
+server.use(express.json());
+server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
+server.use(logger());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger() {
+  return function (req, res, next) {
+    console.log(
+      `Request Method: ${req.method}, Request URL: ${
+        req.url
+      }, Date: ${Date.getTime()}`
+    );
+    next();
+  };
+}
+
+function validateUserId() {
+  return function (req, res, next){
+    
+  }
+}
 
 module.exports = server;
-
-server.listen(3000).json({'Listening on port 3000'})
